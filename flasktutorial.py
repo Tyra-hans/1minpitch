d9fccbb236d66852223b79149201c22e8f901f6d
Tyra-hans <tyrahans17@gmail.com>
2019-10-20 15:38:36 +0300

    creates flash messages that inform the user whether the account nhas been created

--- flasktutorial.py
+++ flasktutorial.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, url_for
+from flask import Flask,render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -27,14 +27,17 @@ def home():
 def about():
     return render_template('about.html', title = 'about' )
 
-@app.route('/register')
+@app.route('/register', methods= ['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title = Register ,form = form)
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = register ,form = form)
 
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title = Login ,form = form)
+    return render_template('login.html', title = login ,form = form)
 if __name__ == '__main__':
     app.run(debug = True)
