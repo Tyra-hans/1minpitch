e054841e85a89fddd8a9a6c70a0e3f2c0117e152
Tyra-hans <tyrahans17@gmail.com>
2019-10-20 14:00:48 +0300

    create dummy data for pitches and loops thru them displaying the pitch and it's content

--- flasktutorial.py
+++ flasktutorial.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
+pitches = [
+    {
+        'author' : 'Tyra Hans' ,
+        'title' : 'Pitch 1',
+        'content' : 'First pitch content',
+        'date_posted' : 'April 20,2019'
+    },
+    {
+        'author' : 'Trevor TheTrev' ,
+        'title' : 'Pitch 2',
+        'content' : 'Second pitch content',
+        'date_posted' : 'May 6,2019'
+    },
+]
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html', posts = pitches)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
