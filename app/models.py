12a10ef9cf4987b889b12ceb937384406288126e
Tyra-hans <tyrahans17@gmail.com>
2019-10-21 11:40:02 +0300

    creates a database called 1 min pitch

--- app/models.py
+++ app/models.py
@@ -8,3 +8,12 @@ class Pitches:
         self.content = content
         self.upvote = upvote
         self.downvote = downvote
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key = True )
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.name}'
